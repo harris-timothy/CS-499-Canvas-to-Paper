267203e5b44108b2ba66638718f909e2d8359ee9
harris-timothy <harris.tim608@gmail.com>
2022-02-07 11:56:33 -0600

    Unzip QTI, Parse XML
    File iteration coming soon(tm)

--- Canvas to Paper/src/cs499/Main.java
+++ Canvas to Paper/src/cs499/Main.java
@@ -1,10 +1,20 @@
 package cs499;
 
+import java.io.IOException;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		ParseQTI qti = new ParseQTI();
+		
+		try {
+			qti.unzip("C:/Users/gamin/Desktop/499/cs-214-03-fa21-intro-discrete-structure-quiz-export.zip", "C:/Users/gamin/Desktop/499/QTITest");
+			qti.xmlParse("C:/Users/gamin/Desktop/499/QTITest/g2fffa9a2e3da438e3f049ef93576c8c2/assessment_meta.xml");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
---
+++ Canvas to Paper/src/cs499/ParseQTI.java
@@ -0,0 +1,128 @@
+package cs499;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.io.File;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+
+public class ParseQTI {
+	// Unzip QTI - Done :)
+	// Open folder
+	// Parse both XMLs (assessment_meta, assessment id)
+	// Iterate through each folder in the .zip
+	
+    /**
+     * Size of the buffer to read/write data
+     */
+    private static final int BUFFER_SIZE = 4096;
+    /**
+     * Extracts a zip file specified by the zipFilePath to a directory specified by
+     * destDirectory (will be created if does not exists)
+     * @param zipFilePath
+     * @param destDirectory
+     * @throws IOException
+     */
+    public void unzip(String zipFilePath, String destDirectory) throws IOException {
+        File destDir = new File(destDirectory);
+        if (!destDir.exists()) {
+            destDir.mkdir();
+        }
+        ZipInputStream zipIn = new ZipInputStream(new FileInputStream(zipFilePath));
+
+        ZipEntry entry = zipIn.getNextEntry();
+        // iterates over entries in the zip file
+        while (entry != null) {
+            String filePath = destDirectory + File.separator + entry.getName();
+            if (!entry.isDirectory()) {
+                // if the entry is a file, extracts it
+                extractFile(zipIn, filePath);
+                System.out.println(filePath);
+            } else {
+                // if the entry is a directory, make the directory
+                File dir = new File(filePath);
+                System.out.println(filePath);
+                dir.mkdir();
+            }
+            zipIn.closeEntry();
+            entry = zipIn.getNextEntry();
+        }
+        zipIn.close();
+    }
+    /**
+     * Extracts a zip entry (file entry)
+     * @param zipIn
+     * @param filePath
+     * @throws IOException
+     */
+    private void extractFile(ZipInputStream zipIn, String filePath) throws IOException {
+        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(filePath));
+        byte[] bytesIn = new byte[BUFFER_SIZE];
+        int read = 0;
+        while ((read = zipIn.read(bytesIn)) != -1) {
+            bos.write(bytesIn, 0, read);
+        }
+        bos.close();
+    }
+    
+    public Document xmlParse(String filePath)
+    {
+    	Document doc = null;
+        try {
+            File inputFile = new File(filePath);
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            doc = dBuilder.parse(inputFile);
+            doc.getDocumentElement().normalize();
+            // Combine with XPath? Look into later
+            // Might want to look at this later:
+            /*NodeList nList = doc.getElementsByTagName("student");
+            System.out.println("----------------------------");
+            
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+               Node nNode = nList.item(temp);
+               System.out.println("\nCurrent Element :" + nNode.getNodeName());
+               
+               if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                  Element eElement = (Element) nNode;
+                  System.out.println("Student roll no : " 
+                     + eElement.getAttribute("rollno"));
+                  System.out.println("First Name : " 
+                     + eElement
+                     .getElementsByTagName("firstname")
+                     .item(0)
+                     .getTextContent());
+                  System.out.println("Last Name : " 
+                     + eElement
+                     .getElementsByTagName("lastname")
+                     .item(0)
+                     .getTextContent());
+                  System.out.println("Nick Name : " 
+                     + eElement
+                     .getElementsByTagName("nickname")
+                     .item(0)
+                     .getTextContent());
+                  System.out.println("Marks : " 
+                     + eElement
+                     .getElementsByTagName("marks")
+                     .item(0)
+                     .getTextContent());
+               }
+            }*/
+         } catch (Exception e) {
+            e.printStackTrace();
+         }
+        System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+		return doc;
+    }
+    
+}
