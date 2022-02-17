7037efccacc3d0967558f025903d40bdb8903de3
elodick <74278780+elodick@users.noreply.github.com>
2022-02-16 18:37:31 -0600

    Initial DB commit
    Converted to Maven project, set up SQLite database, ran JOOQ code generator to make new classes for database access

---
+++ Canvas to Paper/src/cs499/Connect.java
@@ -0,0 +1,37 @@
+package cs499;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author sqlitetutorial.net
+ */
+public class Connect {
+     /**
+     * Connect to a sample database
+     */
+    public static void connect() {
+        Connection conn = null;
+        try {
+            // db parameters
+            String url = "jdbc:sqlite:/db/testdb.sqlite";
+            // create a connection to the database
+            conn = DriverManager.getConnection(url);
+            
+            System.out.println("Connection to SQLite has been established.");
+            
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+}
--- Canvas to Paper/src/cs499/Main.java
+++ Canvas to Paper/src/cs499/Main.java
@@ -1,9 +1,49 @@
 package cs499;
 
+import static cs499.data_classes.Tables.*;
+import static org.jooq.impl.DSL.*;
+
+import java.sql.*;
+
+import org.jooq.*;
+import org.jooq.Record;
+import org.jooq.impl.*;
+
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+        String url = "jdbc:sqlite:db/testdb.sqlite";
+
+        // Connection is the only JDBC resource that we need
+        // PreparedStatement and ResultSet are handled by jOOQ, internally
+        try (Connection conn = DriverManager.getConnection(url)) {
+            DSLContext create = DSL.using(conn, SQLDialect.SQLITE);
+            
+            /*create.insertInto(INSTRUCTOR,
+                    INSTRUCTOR.ID, INSTRUCTOR.NAME, INSTRUCTOR.EMAIL)
+                  .values(3, "Prof. John Smith", "jsmith@fakeemail.com")
+                  .execute();
+            */
+            
+            Result<Record> result = create.select().from(INSTRUCTOR).fetch();
+
+            for (Record r : result) {
+                Integer id = r.getValue(INSTRUCTOR.ID);
+                String name = r.getValue(INSTRUCTOR.NAME);
+                String email = r.getValue(INSTRUCTOR.EMAIL);
+
+                System.out.println("ID: " + id + " name: " + name + " email: " + email);
+            }
+        } 
+
+        // exception handling
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+		
+		
 
 	}
 
