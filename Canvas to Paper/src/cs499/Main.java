2fd04971e064810e9cbddcf4521c987fa4204c67
harris-timothy <harris.tim608@gmail.com>
2022-02-18 21:21:50 -0600

    variable change

--- Canvas to Paper/src/cs499/Main.java
+++ Canvas to Paper/src/cs499/Main.java
@@ -9,13 +9,13 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ParseQTI qti = new ParseQTI();
-		ArrayList<Document> testArray = new ArrayList<Document>();
+		ArrayList<Document> tempArray = new ArrayList<Document>();
 		
 		try {
 			qti.unzip("C:/Users/gamin/Desktop/499/cs-214-03-fa21-intro-discrete-structure-quiz-export.zip", "C:/Users/gamin/Desktop/499/QTITest");
-			testArray = qti.xmlLoop("C:/Users/gamin/Desktop/499/QTITest");
-			System.out.println(testArray.size());
-			for(Document doc : testArray)
+			tempArray = qti.xmlLoop("C:/Users/gamin/Desktop/499/QTITest");
+			System.out.println(tempArray.size());
+			for(Document doc : tempArray)
 			{
 				System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
 			}
