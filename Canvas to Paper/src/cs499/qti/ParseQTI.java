5554ca9f6ec61f2e74fb4ee6a3699b3e923dd7e8
elodick <74278780+elodick@users.noreply.github.com>
2022-04-01 14:39:49 -0500

    hopefully this willwork

--- Canvas to Paper/src/cs499/qti/ParseQTI.java
+++ Canvas to Paper/src/cs499/qti/ParseQTI.java
@@ -19,6 +19,7 @@ import org.w3c.dom.Element;
 
 import cs499.qti.data_mapping.*;
 import cs499.question.AnswerFormatter;
+import cs499.qti.QtiToDB.*;
 import jakarta.xml.bind.JAXBContext;
 import jakarta.xml.bind.JAXBElement;
 import jakarta.xml.bind.JAXBException;
@@ -117,6 +118,7 @@ public class ParseQTI {
     }
         
     /**
+     * Parses a document that uses the QTI schema
      * @param filepath
      * @throws JAXBException
      */
@@ -144,6 +146,7 @@ public class ParseQTI {
        }
     
     /**
+     * Parses a quiz (assessment) from the document
      * @param assessment
      */
     private void parseAssessment(AssessmentType assessment) {
@@ -151,32 +154,41 @@ public class ParseQTI {
     	//store quiz has to go in this function somewhere, so we need all the quiz info to populate to here
     	// arraylist questionIds stores ids to add to quiz_questions table
     	ArrayList<Integer> questionIds = new ArrayList<Integer>();
+    	HashMap<String, String> data = new HashMap<String, String>();
     	
-    	SectionType rootSection = (SectionType) assessment.getSectionrefOrSection().get(FIRST);
+    	data.put("quiz_title", assessment.getTitle());
+    	data.put("quiz_qti_id", assessment.getIdent());
+    	Integer quizId = QtiToDB.storeQuiz(data);
 
+    	SectionType rootSection = (SectionType) assessment.getSectionrefOrSection().get(FIRST);
     	for(Object a: rootSection.getItemrefOrItemOrSectionref()) {
     		if (a instanceof SectionType) {
+    			HashMap<String,String> group = new HashMap<String,String>();
     			//this is where all of the quiz to bank information comes from
-    			System.out.println(((SectionType)a).getTitle()); // quiz title
-    			System.out.println(((SectionType)a).getIdent()); // assessment_qti_id
+    			group.put("quiz_id", quizId.toString());
+    			group.put("group_name", ((SectionType)a).getTitle()); // question group title
     			SelectionType selection = ((SectionType)a).getSelectionOrdering().getSelection().get(FIRST);
-    			System.out.println(selection.getSelectionNumber()); // number to choose from bank
-    			System.out.println(selection.getSourcebankRef()); // bank qti_id
+    			group.put("bank_qti_id", selection.getSourcebankRef());
+    			group.put("pick_count", selection.getSelectionNumber());
 
     			Element test = (Element) removeNull(selection.getSelectionExtension().getContent()).get(FIRST);
-    			System.out.println(test.getFirstChild().getTextContent()); // points per item
+    			group.put("points_per_item",test.getFirstChild().getTextContent()); // points per item
     			
     			//data from here goes into question group table
+    			QtiToDB.storeQuestionGroup(group);
     		}
     		else if (a instanceof ItemType) {
     			ItemType item = (ItemType) a;
-    			questionIds.add(parseItem(item));			
-
+    			questionIds.add(parseItem(item));
     		}
+    	}
+    	for(Integer i: questionIds) {
+    		QtiToDB.storeQuizQuestion(quizId, i);
     	}
     }
     
     /**
+     * Parses a question bank from the document
      * @param bank
      */
     private void parseBank(ObjectbankType bank) {
@@ -184,24 +196,30 @@ public class ParseQTI {
     	//Arraylist of question ids to insert into bank questions table
     	
     	ArrayList<Integer> questionIds = new ArrayList<Integer>();
+    	HashMap<String, String> values = new HashMap<String, String>();
     	
-    	System.out.println(bank.getIdent()); //bank qti_id
+    	values.put("qti_id", bank.getIdent()); //bank qti_id
     	List<QtimetadatafieldType> fields = bank.getQtimetadata().get(FIRST).getQtimetadatafield();
     	for(Object f: fields) {
-			System.out.println(((QtimetadatafieldType) f).getFieldlabel()); //attributes - bank_title
-			System.out.println(((QtimetadatafieldType) f).getFieldentry()); //values - bank_title 	
+    		String label = ((QtimetadatafieldType) f).getFieldlabel();
+    		if(label == "bank_title") {
+    			values.put("bank_title", ((QtimetadatafieldType) f).getFieldentry());
+    		}	
 		}
+    	int bankId = QtiToDB.storeQuestionBank(values);
     	for(Object s: bank.getSectionOrItem()) {
     		if(s instanceof ItemType) {
     			questionIds.add(parseItem((ItemType) s));
     		}
     	}
-    	//function for question to bank association goes here
-    	
+    	for(Integer i: questionIds) {
+    		QtiToDB.storeBankQuestion(bankId, i);
+    	}    	
     }
     
     
     /**
+     * Parses the item section of the document and returns the question id as an integer
      * @param item
      * @return
      */
@@ -211,43 +229,58 @@ public class ParseQTI {
     	//insert ids into quiz_to_question or bank_questions as applicable
     	int questionId = 0;
     	
-    	HashMap<String, String> values = new HashMap<String, String>();
-    	values.put("name", item.getTitle());
-    	
+    	HashMap<String, String> questionValues = new HashMap<String, String>();
+    	questionValues.put("name", item.getTitle());
+    	questionValues.put("qti_id", item.getIdent());    	
     	
     	List<QtimetadatafieldType> fields = item.getItemmetadata().getQtimetadata().get(FIRST).getQtimetadatafield();
     	for(Object f: fields) {
 			String label = ((QtimetadatafieldType) f).getFieldlabel();
 			if(label == "question_type") {
-				values.put("question_type", ((QtimetadatafieldType) f).getFieldentry());
+				questionValues.put("question_type", ((QtimetadatafieldType) f).getFieldentry());
 			}
 			else if (label == "points_possible") {
-				values.put("points_possible", ((QtimetadatafieldType) f).getFieldentry());
+				questionValues.put("points_possible", ((QtimetadatafieldType) f).getFieldentry());
 			}			
 		}
-    	ArrayList<Object> responseList;
+    	ArrayList<Object> responseList = new ArrayList<Object>();
 		for(Object o : item.getPresentation().getMaterialOrResponseLidOrResponseXy()) {
 			if(o instanceof MaterialType) {
-				values.put("description", parseMaterial((MaterialType) o));
+				questionValues.put("description", parseMaterial((MaterialType) o));
 			}
 			else if (o instanceof ResponseLidType) {
-				responseList = parseResponseLid((ResponseLidType) o);
+				responseList.addAll(parseResponseLid((ResponseLidType) o));
 				//returns arraylist of hashmaps
-			}						
+				//response ident
+				//choices
+			}
+			else if (o instanceof ResponseStrType) {
+				HashMap<String,String> responseInfo = new HashMap<String,String>();
+				responseInfo.put("answer_ident",((ResponseStrType) o).getIdent());
+				responseList.add(responseInfo);
+				//returns ident for correct answer
+			}
 		}
-		ArrayList<Object> processingList;
+		
+		ArrayList<Object> correctAnswers = new ArrayList<Object>();
 		for(Object r: item.getResprocessing()) {
-			processingList = parseResprocessing((ResprocessingType) r);
+			correctAnswers.addAll(parseResprocessing((ResprocessingType) r));
 			//returns arraylist of hashmaps
-		}
-		HashMap<String, String> feedback;
+			//response ident to match responselist
+			//response value to match choice value of correct answer
+		}		
 		for(Object f: item.getItemfeedback()) {
+			HashMap<String, String> feedback;
 			((ItemfeedbackType) f).getIdent(); //itemfeedback ident
 			//if conditionvar other
 			//displayfeedback linkrefid = ident
 			feedback = parseItemFeedback((ItemfeedbackType) f);
+			correctAnswers.add(feedback);
 		}
 		
+		String answers = QtiToDB.parseAnswers(correctAnswers, responseList, questionValues);
+		questionValues.put("answers", answers);
+		questionId = QtiToDB.storeQuestion(questionValues);
 		//use hashmap to store question
 		//get id from stored question
 		return questionId;
@@ -255,14 +288,20 @@ public class ParseQTI {
     	
     }
     
+    /**
+     * Parses the itemfeedback section into a single map
+     * @param feedback
+     * @return
+     */
     private HashMap<String, String> parseItemFeedback(ItemfeedbackType feedback) {
     	
     	HashMap<String, String> answer = new HashMap<String, String>();
+    	answer.put("answer_ident", feedback.getIdent());
     	for(Object o: feedback.getFlowMatOrMaterialOrSolution()) {
     		if(o instanceof FlowMatType) {
     			for(Object k: ((FlowMatType) o).getFlowMatOrMaterialOrMaterialRef()) {
     				if(k instanceof MaterialType) {
-    					answer.put("",parseMaterial((MaterialType) o));
+    					answer.put("answer_value",parseMaterial((MaterialType) k));
     				}
     			}    			   			
     		}
@@ -271,12 +310,17 @@ public class ParseQTI {
     }
     
     /**
+     * Parses the responselid section of the document into a list of maps
      * @param responseLid
+     * @return
      */
     @SuppressWarnings("rawtypes")
 	private ArrayList<Object> parseResponseLid(ResponseLidType responseLid) {
     	
     	ArrayList<Object> data = new ArrayList<Object>();
+    	
+    	responseLid.getIdent();
+    	
     	List<JAXBElement<?>> list = removeNull(responseLid.getContent());    	
     	
 		for(JAXBElement<?> e: list) {
@@ -288,20 +332,22 @@ public class ParseQTI {
 				
 			}
 			else if (e.getValue() instanceof RenderChoiceType) {
+				HashMap<String,String> choices = new HashMap<String,String>();
 				RenderChoiceType render = (RenderChoiceType) e.getValue();
 				for(Object val: render.getMaterialOrMaterialRefOrResponseLabel()) {
 					if (val instanceof ResponseLabelType) {
 						
-						map.put("answer_ident", ((ResponseLabelType) val).getIdent());
+						choices.put("answer_ident", ((ResponseLabelType) val).getIdent());
 					 
 						List<Serializable> label = removeNull(((ResponseLabelType) val).getContent());
 						for(Object t: label) {
 							if(((JAXBElement)t).getValue() instanceof MaterialType) {
-								map.put("answer_value", parseMaterial((MaterialType) ((JAXBElement)t).getValue())); 
+								choices.put("answer_value", parseMaterial((MaterialType) ((JAXBElement)t).getValue())); 
 							}
 						}								
 					}
 				}
+				
 			}
 			data.add(map);
 		}
@@ -310,6 +356,7 @@ public class ParseQTI {
     }
 	
 	/**
+	 * Parses a material entry into a string, and removes any html formatting
 	 * @param material
 	 * @return
 	 */
@@ -325,7 +372,9 @@ public class ParseQTI {
 	}
 	
 	/**
+	 * Parses the resprocessing section of the qti document into a list of maps
 	 * @param resprocessing
+	 * @return
 	 */
 	private ArrayList<Object> parseResprocessing(ResprocessingType resprocessing) {
 		ArrayList<Object> list = new ArrayList<Object>();
@@ -350,6 +399,7 @@ public class ParseQTI {
 	
 	
 	/**
+	 * Helper method to remove null or whitespace only entries from lists
 	 * @param <T>
 	 * @param list
 	 * @return
--- Canvas to Paper/src/cs499/qti/QtiToDB.java
+++ Canvas to Paper/src/cs499/qti/QtiToDB.java
@@ -10,6 +10,7 @@ import static cs499.question.QuestionType.*;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import org.jooq.DSLContext;
@@ -22,7 +23,8 @@ import cs499.question.QuestionType;
 
 public class QtiToDB {
 	
-	public void storeQuiz(HashMap<String, String> data) {
+	public static int storeQuiz(HashMap<String, String> data) {
+		int quizId = 0;
 		try (Connection conn = DriverManager.getConnection(DataHelper.ENV.get("DB_URL"))) {
 			DSLContext create = DSL.using(conn, SQLDialect.SQLITE);
 			
@@ -34,36 +36,31 @@ public class QtiToDB {
 			if(exists == null) {
 				create.insertInto(QUIZ,
 						QUIZ.QTI_ID,
-						QUIZ.NAME,
-						QUIZ.DESCRIPTION,
-						QUIZ.POINTS_POSSIBLE,
-						QUIZ.DUE_DATE)
-				.values(data.get("qti_id"),
-						data.get("name"),
-						data.get("description"),
-						Float.parseFloat(data.get("points_possible")),
-						data.get("due_date"))
-				.execute();
-				
+						QUIZ.NAME)
+				.values(data.get("quiz_qti_id"),
+						data.get("quiz_title"))
+				.execute();				
 			}
 			else {
 				create.update(QUIZ)
 				.set(QUIZ.NAME, data.get("name"))
-				.set(QUIZ.DESCRIPTION, data.get("description"))
-				.set(QUIZ.POINTS_POSSIBLE, Float.parseFloat(data.get("points_possible")))
-				.set(QUIZ.DUE_DATE, data.get("due_date"))
 				.where(QUIZ.QTI_ID.eq(data.get("qti_id")))
 				.execute();
-				
 			}
+			
+			quizId = create.select(QUIZ.ID)
+					.from(QUIZ)
+					.where(QUIZ.QTI_ID.eq(data.get("quiz_qti_id")))
+					.fetchOne(QUIZ.ID);
 		}
 		catch(Exception e) {
 			e.printStackTrace();
 		}
+		return quizId;
 		
 	}
 	
-	public int storeQuestion(HashMap<String, String> data) {
+	public static int storeQuestion(HashMap<String, String> data) {
 		int questionId = 0;
 		try (Connection conn = DriverManager.getConnection(DataHelper.ENV.get("DB_URL"))) {
 			DSLContext create = DSL.using(conn, SQLDialect.SQLITE);     
@@ -79,12 +76,14 @@ public class QtiToDB {
 						QUESTION.NAME,
 						QUESTION.DESCRIPTION,
 						QUESTION.TYPE,
-						QUESTION.ANSWERS)
+						QUESTION.ANSWERS,
+						QUESTION.POINTS_POSSIBLE)
 				.values(data.get("qti_id"),
 						data.get("name"),
 						data.get("description"),
 						data.get("question_type"),
-						data.get("answers"))
+						data.get("answers"),
+						Float.parseFloat(data.get("points_possible")))
 				.execute();
 
 			}
@@ -94,6 +93,7 @@ public class QtiToDB {
 				.set(QUESTION.DESCRIPTION, data.get("description"))
 				.set(QUESTION.TYPE, data.get("question_type"))
 				.set(QUESTION.ANSWERS, data.get("answers"))
+				.set(QUESTION.POINTS_POSSIBLE, Float.parseFloat(data.get("points_possible")))
 				.where(QUESTION.QTI_ID.eq(data.get("qti_id")))
 				.execute();
 			}
@@ -101,9 +101,7 @@ public class QtiToDB {
 			questionId = create.select(QUESTION.ID)
 			.from(QUESTION)
 			.where(QUESTION.QTI_ID.eq(data.get("qti_id")))
-			.fetchOne(QUESTION.ID);
-			
-			
+			.fetchOne(QUESTION.ID);			
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -111,7 +109,7 @@ public class QtiToDB {
 		return questionId;
 	}
 	
-	public int storeQuestionBank(HashMap<String, String> data) {
+	public static int storeQuestionBank(HashMap<String, String> data) {
 		int bankId = 0;
 		try (Connection conn = DriverManager.getConnection(DataHelper.ENV.get("DB_URL"))) {
 			DSLContext create = DSL.using(conn, SQLDialect.SQLITE);     
@@ -126,7 +124,7 @@ public class QtiToDB {
 						QUESTION_BANK.QTI_ID,
 						QUESTION_BANK.NAME)
 				.values(data.get("qti_id"),
-						data.get("name"))
+						data.get("bank_title"))
 				.execute();
 			}
 			
@@ -143,7 +141,7 @@ public class QtiToDB {
 		
 	}
 	
-	public void storeQuizQuestion(int quizId, int questionId) {
+	public static void storeQuizQuestion(int quizId, int questionId) {
 		try (Connection conn = DriverManager.getConnection(DataHelper.ENV.get("DB_URL"))) {
 			DSLContext create = DSL.using(conn, SQLDialect.SQLITE);     
 
@@ -167,7 +165,7 @@ public class QtiToDB {
 		}		
 	}
 	
-	public void storeBankQuestion(int bankId, int questionId) {
+	public static void storeBankQuestion(int bankId, int questionId) {
 		try (Connection conn = DriverManager.getConnection(DataHelper.ENV.get("DB_URL"))) {
 			DSLContext create = DSL.using(conn, SQLDialect.SQLITE);     
 
@@ -192,14 +190,20 @@ public class QtiToDB {
 		
 	}
 	
-	public void storeQuestionGroup(HashMap<String, Integer> data) {
+	public static void storeQuestionGroup(HashMap<String, String> data) {
 		try (Connection conn = DriverManager.getConnection(DataHelper.ENV.get("DB_URL"))) {
-			DSLContext create = DSL.using(conn, SQLDialect.SQLITE);     
+			DSLContext create = DSL.using(conn, SQLDialect.SQLITE);
+			
+			int bankId = create.select(QUESTION_BANK.ID)
+					.from(QUESTION_BANK)
+					.where(QUESTION_BANK.QTI_ID.eq(data.get("bank_qti_id")))
+					.fetchOne(QUESTION_BANK.ID);
 
 			Record exists = create.select()
 					.from(QUESTION_GROUP)
-					.where(QUESTION_GROUP.QUIZ_ID.eq(data.get("quiz_id")))
-					.and(QUESTION_GROUP.QUESTION_BANK_ID.eq(data.get("bank_id")))
+					.where(QUESTION_GROUP.QUIZ_ID.eq(Integer.parseInt(data.get("quiz_id"))))
+					.and(QUESTION_GROUP.QUESTION_BANK_ID.eq(bankId))
+					.and(QUESTION_GROUP.NAME.eq(data.get("group_name")))
 					.fetchOne();
 
 			if(exists == null) {
@@ -207,11 +211,13 @@ public class QtiToDB {
 						QUESTION_GROUP.QUIZ_ID,
 						QUESTION_GROUP.QUESTION_BANK_ID,
 						QUESTION_GROUP.PICK_COUNT,
-						QUESTION_GROUP.QUESTION_POINTS)
-				.values(data.get("quiz_id"),
-						data.get("bank_id"),
-						data.get("selection_number"),
-						data.get("points_per_item").floatValue())
+						QUESTION_GROUP.QUESTION_POINTS,
+						QUESTION_GROUP.NAME)
+				.values(Integer.parseInt(data.get("quiz_id")),
+						bankId,
+						Integer.parseInt(data.get("pick_count")),
+						Float.parseFloat(data.get("points_per_item")),
+						data.get("group_name"))
 				.execute();
 			}
 
@@ -220,16 +226,32 @@ public class QtiToDB {
 		}		
 	}
 	
-	public String parseAnswers(HashMap<String, String> correct, HashMap<String, String> allAnswers, QuestionType type) {
+	public static String parseAnswers(ArrayList<Object> correctAnswers, ArrayList<Object> allChoices, HashMap<String, String> questionInfo) {
+		
+		QuestionType type = valueOf(questionInfo.get("question_type"));
+		
 		switch(type) {
 		case MATCHING:
 		case MULTIPLE_ANSWERS:
 			//matching stuff
+			//answer_name = left
+			//to find matching right
+			//get the corresponding answer_value
+			//where answer_ident from correctanswers matches ident from allchoices
 			break;
 		case MULTIPLE_CHOICE:
 		case TRUE_FALSE:
 		case MULTIPLE_DROPDOWNS:
 			//multiple choice stuff
+			//get "answer_ident" from correctanswers(responselid)
+			//use that ident to find value from allchoices
+			//corretanswers->ident should = allchoices->ident
+			//answer text = "answer_value"
+			//string for correct answer
+			//to find choices
+			//allchoices -> answer_value
+			//arraylist for options
+			
 			break;
 		case CALCULATED:
 		case ESSAY:		
@@ -237,20 +259,24 @@ public class QtiToDB {
 		case NUMERICAL:
 		case SHORT_ANSWER:
 			//single answer stuff
+			//get "answer_ident" from correctanswers(responselid or responsestr)
+			//use that ident to find value from allchoices
+			//correctanswers->ident should = allchoices->ident
+			//answer text = "answer_value"
+			
 			break;
 		case TEXT_ONLY:
 		case FILE_UPLOAD:
-			// no answers for these - maybe put in with single answer?
-		
+			// no answers for these		
 		default:
-			break;
-			
+			break;		
 		}
 		
 		
 		
 		return null;
 	}
+	
 	
 
 }
