e912821c6c14891572469eff7f29cd562883de98
Jeremiah Brewer <Jeremiahgbrewer@gmail.com>
2022-04-08 20:02:41 -0500

    Added Export Baseline Functionality for others to review and help with

--- Canvas to Paper/src/cs499/screens/WelcomeScreen.java
+++ Canvas to Paper/src/cs499/screens/WelcomeScreen.java
@@ -3,10 +3,13 @@ package cs499.screens;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JInternalFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
@@ -23,41 +26,62 @@ import cs499.gui_utils.MenuBuilder;
 
 public class WelcomeScreen {
     public WelcomeScreen(){
-        String frame_title = "CS 499-01 Spring 2022 CtPP Project Prototype-01";
+        //Initialize starting simple variables
+		String frame_title = "CS 499-01 Spring 2022 CtPP Project Prototype-01";
 		String logo_icon_path = "Canvas to Paper/lib/images/logo_icon.png";
-		JFrame frame;
-
+		
 		//Initialize gui helper classes
 		MenuBuilder menu = new MenuBuilder();
 		FileExplorer explorer = new FileExplorer();
-
+		FrameBuilder maker = new FrameBuilder();
+		
 		//Create Menu Bar
 		JMenuBar menu_bar = new JMenuBar();
 
-		//Create the File Menu
+		//Initialize Frames
+		JFrame frame = maker.buildFrame(
+			frame_title, 
+			JFrame.DISPOSE_ON_CLOSE, 
+			1000, 800, 
+			menu_bar, 
+			logo_icon_path
+		);
+		frame.setLayout(new GridBagLayout());
+		GridBagConstraints constraints = new GridBagConstraints();
+	
+		JFrame export_frame = maker.buildFrame(
+			"Please Select Quizzes/QuizBanks to Export", 
+			JFrame.DISPOSE_ON_CLOSE, 
+			800, 
+			600,
+			null, 
+			logo_icon_path);
+		export_frame.setLayout(new GridBagLayout());
+		GridBagConstraints export_constraints = new GridBagConstraints();
+		
+		//Fill the Menu Bar:
 		JMenu file_menu = menu.buildMenu("File", KeyEvent.VK_F);
+		menu_bar.add(file_menu);
 		
-		//Create File -> Import QTI File
+		//File -> Import QTI Files
 		JMenuItem import_mi = menu.buildMenuItem("Import QTI Files", KeyEvent.VK_I, file_menu);
-		
-		//Create Import QTI File Action Handler
 		class ImportAction implements ActionListener {
 			public void actionPerformed(ActionEvent e) {
 				//Initialize variables
 				String[] ImportZip;
 				String[] ErrorArray = {"Error"};
 				ParseQTI qti = new ParseQTI();
-
+				
 				//Loop to ensure proper selection or cancel selection
 				do {
 					//Check for errors and retry if caught
 					do {
 						ImportZip = explorer.FilesSelect();
 					} while (ImportZip == ErrorArray);
-
+					
 					//Check for cancellations and cancel if caught
 					if (ImportZip == null) return;
-
+					
 					//Check for non .zip files and retry if caught
 					for (String filepath : ImportZip) {
 						if (!filepath.contains(".zip")) {
@@ -81,52 +105,124 @@ public class WelcomeScreen {
 		}
 		import_mi.addActionListener(new ImportAction());
 		
-		//Create File -> Export QTI File
+		//File -> Export QTI File
 		JMenuItem export_mi = menu.buildMenuItem("Export QTI File", KeyEvent.VK_E, file_menu);
-		
-		//Create Export QTI File Action Handler
 		class ExportAction implements ActionListener {
 			public void actionPerformed(ActionEvent e) {
-				//TODO: Export behavior
-				System.out.println("Export QTI File Button Pressed.");
+				
+				// For teammates:
+				/* TODO: 
+				* Will need a way to get an array/arraylist/iteratable list of each quiz 
+				* and each quiz bank they could export, each item in the list 
+				* holding the following information:
+				*	- ID/Name of quiz
+				*	- Type of item (Quiz Bank/Quiz)
+				*	- Any other information needed for the following to do block about exporting a modified version of this list
+				*/
+				// ListObject QQB = new someobject.listQuizStuff()
+				
+				// TODO: Make a second list to be modified
+				// ListObject Modified_QQB = new ListObject;
+				
+				JButton export_cfm_btn = new JButton("Export");
+				class ExportConfirmAction implements ActionListener {
+					public void actionPerformed(ActionEvent e) {
+						export_frame.dispose();
+						//TODO: Pass Modified_QQB to the export qti function
+					}
+				}
+				export_cfm_btn.addActionListener(new ExportConfirmAction());
+				
+				JButton export_cnl_btn = new JButton("Cancel");
+				class ExportCancelAction implements ActionListener {
+					public void actionPerformed(ActionEvent e) {
+						export_frame.dispose();
+						//TODO: Ensure frame is properly reset, not just hidden and reshown later.
+					}
+				}
+				export_cnl_btn.addActionListener(new ExportCancelAction());
+				
+				JPanel qqb_listing = new JPanel();
+				qqb_listing.setLayout(new GridBagLayout());
+				GridBagConstraints qqb_list_constraints = new GridBagConstraints();
+				
+				//TODO: Retrieve length of QQB
+				int QQB_length = 100;
+				for (int i = 0; i < QQB_length; i++){
+					//TODO: Make a checkbox for the button to indicate presence in modified list
+					
+					//TODO: Get ID/Name of Q/QB to use in display
+					//String q_qb_name = QQB[i].name
+					String q_qb_name = "Element " + i;
+					JButton q_qb_btn = new JButton(q_qb_name);
+					final int q_qb_index = i;
+					class ExportQuizSelectorAction implements ActionListener {
+						public void actionPerformed(ActionEvent e) {
+							//TODO: Check if QQB[q_qb_index] is in Modified_QQB, and if not, add it to Modified_QQB and check q_qb_checkbox
+							//TODO: Else, remove QQB[q_qb_index] from Modified_QQB and de-check q_qb_checkbox
+						}
+					}
+					q_qb_btn.addActionListener(new ExportQuizSelectorAction());
+
+					JPanel q_qb_panel = new JPanel();
+					//q_qb_panel.add(q_qb_checkbox);
+					q_qb_panel.add(q_qb_btn);
+
+					qqb_list_constraints.gridx = 0;
+					qqb_list_constraints.gridy = i;
+					qqb_listing.add(q_qb_panel, qqb_list_constraints);
+				}
+				
+				//Add elements to frame
+				export_constraints.fill = GridBagConstraints.BOTH;
+				
+				export_constraints.gridx = 0;
+				export_constraints.weightx = 0.1;
+				export_constraints.gridy = 0;
+				export_constraints.weighty = 0.1;
+				export_frame.add(new JLabel(), export_constraints);
+				
+				export_constraints.gridx = 0;
+				export_constraints.weightx = 0.1;
+				export_constraints.gridy = 2;
+				export_constraints.weighty = 0.1;
+				export_frame.add(export_cfm_btn, export_constraints);
+
+				export_constraints.gridx = 2;
+				export_constraints.weightx = 0.1;
+				export_constraints.gridy = 2;
+				export_constraints.weighty = 0.1;
+				export_frame.add(export_cnl_btn, export_constraints);
+
+				export_constraints.gridx = 1;
+				export_constraints.weightx = 0.9;
+				export_constraints.gridy = 1;
+				export_constraints.weighty = 0.9;
+				export_frame.add(new JScrollPane(qqb_listing), export_constraints);
+
+				export_frame.setVisible(true);
 			}
 		}
 		export_mi.addActionListener(new ExportAction());
 
-		//Create File -> Select Test
+		//File -> Select Test
 		JMenuItem select_mi = menu.buildMenuItem("Select Test", KeyEvent.VK_S, file_menu);
-
-		//Add File Menu to Menu Bar
-		menu_bar.add(file_menu);
-
-		//Initialize the Welcome Screen frame of the application
-		FrameBuilder maker = new FrameBuilder();
-		frame = maker.buildFrame(
-			frame_title, 
-			JFrame.DISPOSE_ON_CLOSE, 
-			1000, 800, 
-			menu_bar, 
-			logo_icon_path
-		);
-		frame.setLayout(new GridBagLayout());
-		
-		//Create Select Test Action Handler
 		class SelectAction implements ActionListener {
 			public void actionPerformed(ActionEvent e) {
 				new SelectQuizScreen();
-				frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));;
+				frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
 			}
 		}
 		select_mi.addActionListener(new SelectAction());
+		
+		//TODO: For someone else, need to implement a login functionality to handle login, authentication, and any username/pfp stuff. 
+		//		Alternatively, we can scrap this and just not display a username or pfp or anything like that. Relatively easy to cut.
 
-		//Create Profile Picture Image
-		String pfp_path = "";
-		pfp_path = logo_icon_path;
-		//TODO: Set pfp_path to username's profile picture
-
+		//TODO: For someone else, get the username somehow
 		String username = "";
-		username = "USERNAME";
-		//TODO: Get the username somehow
+
+		//TODO: For someone else, get the username's profile picture
+		String pfp_path = "";
 
 		//Create Import QTI File Button
 		JButton import_btn = new JButton("Import QTI Files");
@@ -139,9 +235,6 @@ public class WelcomeScreen {
 		//Create Create New Test Button
 		JButton new_test_btn = new JButton("Create New Test");
 
-		//Create Constraints Guide
-		GridBagConstraints constraints = new GridBagConstraints();
-
 		//Add elements to frame
 		constraints.fill = GridBagConstraints.BOTH;
 		
@@ -149,6 +242,12 @@ public class WelcomeScreen {
 		
 		//Add padding around of 10% top, 10% left, 20% bottom, 10% right, and a middle divider of 10%
 		constraints.gridx = 0;
+		constraints.gridy = 6;
+		constraints.weightx = 0.1;
+		constraints.weighty = 0.1;
+		frame.add(new JLabel(), constraints);
+
+		constraints.gridx = 2;
 		constraints.gridy = 0;
 		constraints.weightx = 0.1;
 		constraints.weighty = 0.1;
