f27224d93b7e7d4139b9c73a00a21810fed249bf
Emily Lodick <74278780+elodick@users.noreply.github.com>
2022-04-14 23:44:56 -0500

    Revert "Jeremiah branch"

--- Canvas to Paper/src/cs499/Main.java
+++ Canvas to Paper/src/cs499/Main.java
@@ -1,9 +1,10 @@
 package cs499;
 
-import cs499.screens.WelcomeScreen;
-
 public class Main {
+
 	public static void main(String[] args) {
-		new WelcomeScreen();
+		// TODO Auto-generated method stub
+
 	}
+
 }
--- Canvas to Paper/src/cs499/screens/EditQuestionScreen.java
+++
@@ -1,9 +0,0 @@
-package cs499.screens;
-
-import cs499.question.Question;
-
-public class EditQuestionScreen {
-    public EditQuestionScreen(Question question){
-        
-    }
-}
