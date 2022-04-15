0c9336ffab00b686e191218a11fb091f86f41049
harris-timothy <harris.tim608@gmail.com>
2022-04-14 21:36:16 -0500

    Quiz Info Panel Skeleton

--- Canvas to Paper/src/cs499/screens/EditQuizScreen.java
+++ Canvas to Paper/src/cs499/screens/EditQuizScreen.java
@@ -1,11 +1,17 @@
 package cs499.screens;
 
+import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTextField;
 
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 
 import cs499.Quiz;
@@ -13,6 +19,15 @@ import cs499.gui_utils.FrameBuilder;
 
 public class EditQuizScreen {
     private JFrame frame;
+    private JLabel quizLabel;
+    private JTextField quizField;
+    private JLabel courseLabel;
+    private JTextField courseField;
+    private JLabel dateLabel;
+    private JFormattedTextField dateField;
+    private JLabel instructorLabel;
+    private JTextField instructorField;
+    
     public EditQuizScreen(Quiz quiz){
         String frame_title = "CS 499-01 Spring 2022 CtPP Project Prototype-01";
         String logo_icon_path = "Canvas to Paper/lib/images/logo_icon.png";
@@ -30,8 +45,48 @@ public class EditQuizScreen {
         GridBagConstraints constraints = new GridBagConstraints();
 
         JPanel quiz_info_panel = new JPanel();
-
-        //TODO: Add quiz info to the quiz_info_panel
+        quiz_info_panel.setLayout(null);
+        
+        this.quizLabel = new JLabel();
+        quizLabel.setText("Quiz Name");
+        quizLabel.setBounds(10,0,100,30);
+        
+        this.quizField = new JTextField();
+        quizField.setBounds(10, 30, 325, 20);
+        
+        this.courseLabel = new JLabel();
+        courseLabel.setText("Course Number");
+        courseLabel.setBounds(350,0,100,30);
+        
+        this.courseField = new JTextField();
+        courseField.setBounds(350, 30, 120, 20);
+        
+        this.dateLabel = new JLabel();
+        dateLabel.setText("Date (MM/DD/YYYY)");
+        dateLabel.setBounds(10, 55, 150, 30);
+        
+        DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+        this.dateField = new JFormattedTextField(dateFormat);
+        dateField.setHorizontalAlignment(JTextField.RIGHT);
+        dateField.setBounds(10, 85, 125, 20);
+        
+        this.instructorLabel = new JLabel();
+        instructorLabel.setText("Instructor");
+        instructorLabel.setBounds(150,55,150,30);
+        
+        this.instructorField = new JTextField();
+        instructorField.setBounds(150,85,200,20);
+        
+        //TODO: Add functionality of each field
+        
+        quiz_info_panel.add(quizLabel);
+        quiz_info_panel.add(quizField);
+        quiz_info_panel.add(courseLabel);
+        quiz_info_panel.add(courseField);
+        quiz_info_panel.add(dateLabel);
+        quiz_info_panel.add(dateField);
+        quiz_info_panel.add(instructorLabel);
+        quiz_info_panel.add(instructorField);
 
         JPanel questions_panel = new JPanel();
 
