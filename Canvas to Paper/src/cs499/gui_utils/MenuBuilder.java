8f46f4f923c62eda8b71d52a17e8e850da74b5b2
Jeremiah Brewer <Jeremiahgbrewer@gmail.com>
2022-04-30 20:34:58 -0500

    Merge with Emily's Branch

---
+++ Canvas to Paper/src/cs499/gui_utils/MenuBuilder.java
@@ -0,0 +1,17 @@
+package cs499.gui_utils;
+
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+
+public class MenuBuilder {
+    public JMenu buildMenu(String title, int mnemonic){
+        JMenu menu = new JMenu(title);
+        menu.setMnemonic(mnemonic);
+        return menu;
+    }
+    public JMenuItem buildMenuItem(String title, int mnemonic, JMenu supermenu){
+        JMenuItem menu_item = new JMenuItem(title, mnemonic);
+        supermenu.add(menu_item);
+        return menu_item;
+    }
+}
